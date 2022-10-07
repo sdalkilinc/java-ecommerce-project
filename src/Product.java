ba593068beb388a4df81d611f82e754021684bfd
Sumeye <dalkilincsumeye@gmail.com>
2022-10-07 16:34:03 -0400

    Product class is created

---
+++ src/Product.java
@@ -0,0 +1,45 @@
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Product {
+
+    private UUID id;
+    private String name;
+    private Double price;
+    private Integer stock;
+    private Integer remainingStock;
+    private UUID categoryId;
+
+    public Product(UUID id, String name, Double price, Integer stock, Integer remainingStock, UUID categoryId) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.remainingStock = remainingStock;
+        this.categoryId = categoryId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public Integer getRemainingStock() {
+        return remainingStock;
+    }
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+}
