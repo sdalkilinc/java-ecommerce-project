6d8e7b48219fb6db77d09b5752d6c42a68d5178d
Sumeye <dalkilincsumeye@gmail.com>
2022-10-07 14:53:01 -0400

    Category and Electronic classes are created

---
+++ src/category/Category.java
@@ -0,0 +1,14 @@
+package category;
+
+import java.util.UUID;
+
+public class Category {
+
+    private UUID id;
+    private String name;
+
+    public Category(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ src/category/Electronic.java
@@ -0,0 +1,10 @@
+package category;
+
+import java.util.UUID;
+
+public class Electronic extends Category{
+
+    public Electronic(UUID id, String name) {
+        super(id, name);
+    }
+}
