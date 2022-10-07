fbcc8b613bf2172e07aa6005a1dff081dfe52d4d
Sumeye <dalkilincsumeye@gmail.com>
2022-10-07 17:50:38 -0400

    Discount class is added

---
+++ src/discount/Discount.java
@@ -0,0 +1,30 @@
+package discount;
+
+import java.util.UUID;
+
+public abstract class Discount {
+
+    private UUID id;
+    private String name;
+    private Double thresholdAmount;
+
+    public Discount(UUID id, String name, Double thresholdAmount) {
+        this.id = id;
+        this.name = name;
+        this.thresholdAmount = thresholdAmount;
+    }
+
+    public abstract Double calculateCartAmountAfterDiscountApplied(Double amount);
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getThresholdAmount() {
+        return thresholdAmount;
+    }
+}
